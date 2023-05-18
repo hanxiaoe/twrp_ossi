33008657bc51394d295a9f7f3b8324ebaa39681d
hanxiaoe <2282851588@qq.com>
2023-05-18 21:31:54 +0800

    11

---
+++ twrp_ossi.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from ossi device
+$(call inherit-product, device/oplus/ossi/device.mk)
+
+PRODUCT_DEVICE := ossi
+PRODUCT_NAME := twrp_ossi
+PRODUCT_BRAND := oplus
+PRODUCT_MODEL := ossi
+PRODUCT_MANUFACTURER := oplus
+
+PRODUCT_GMS_CLIENTID_BASE := android-oplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="lito-user 12 RKQ1.211103.002 1678802601354 release-keys"
+
+BUILD_FINGERPRINT := qti/lito/lito:12/RKQ1.211103.002/1678802601354:user/release-keys
